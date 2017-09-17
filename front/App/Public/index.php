e835c220143ad5260f55a7cca9f7575d5b77a687
luoyuxiang <luoyuxiang@luoyuxiangdembp.lan>
2017-09-17 22:45:28 +0800

    update display  error

--- front/App/Public/index.php
+++ front/App/Public/index.php
@@ -18,10 +18,8 @@ define("DS", DIRECTORY_SEPARATOR);
 define("FRAMEWORK_PATH", dirname(dirname(dirname(__DIR__))) . DS .'Library/Nig' . DS);
 define("APPLICATION_PATH", dirname(dirname(__DIR__)) . DS .'App' . DS);
  
-ini_set('display_errors',1);            //错误信息
-ini_set('display_startup_errors',1);    //php启动错误信息
-error_reporting(-1);                    //打印出所有的 错误信息
-ini_set('error_log', dirname(__FILE__) . '/error_log.txt'); //将出错信息输出到一个文本文件
+ini_set('display_errors',1);            
+error_reporting(E_ALL);                     
  
 include FRAMEWORK_PATH .'Nig.php'; 
 $nig = \Nig\Nig::getInstance(APPLICATION_PATH . 'Config/Config.php');
