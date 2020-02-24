7bc35684e7fe6ff742a41a5dd19de31a19a7f147
luo <269724033@qq.com>
2020-02-24 23:09:43 +0800

    change route

--- front/App/Public/index.php
+++ front/App/Public/index.php
@@ -8,38 +8,49 @@
 define("DS", DIRECTORY_SEPARATOR);
 define("FRAMEWORK_PATH", dirname(dirname(dirname(__DIR__))) . DS .'Library' . DS . 'Ngx' . DS);
 define("APPLICATION_PATH", dirname(dirname(__DIR__)) . DS .'App' . DS);
+define('UTF8_ENABLED', FALSE); 
+error_reporting(E_ALL & ~E_NOTICE);    
+
+$charset = 'UTF-8';
+ini_set('default_charset', $charset); //设置默认字符集 
+ini_set('date.timezone','Asia/Shanghai');
+if (extension_loaded('mbstring'))
+{
+    @ini_set('mbstring.internal_encoding', $charset); 
+    mb_substitute_character('none');
+}
 
 
-error_reporting(E_ALL);      
+function _main()
+{
+    try {
+        include FRAMEWORK_PATH . 'Ngx.php';
+        Ngx\Import::addLibrary(FRAMEWORK_PATH, 'Ngx');
+        Ngx\Import::addLibrary(APPLICATION_PATH, 'App');
+        Ngx\Import::addLibrary(dirname(APPLICATION_PATH) . DS .'Sdk' . DS, 'Sdk'); 
 
-if (php_sapi_name() == 'cli-server') {
-    if (preg_match('/\.(?:png|jpg|jpeg|gif|js|css|woff|woff2|ttf)(\?.*)?$/', $_SERVER['REQUEST_URI'])) {
-        return false;
+        Ngx\Ngx::getInstance()
+            ->init(APPLICATION_PATH . 'Config/Main.php')
+            ->run();
     }
-}
-
-if (PHP_SAPI === 'cli')
-{
-    $_SERVER['PATH_INFO'] = $_SERVER['argv'][1];
-}
-
-foreach ($_POST as $k => $v)
-{
-    if (is_string($v))
+    catch (\Exception $e)
     {
-        $_POST[$k] = addslashes($v);
+        $errMsg = '<html><head><title>error!</title></head><body bgcolor="white" text="black"><center>';
+        $errMsg .= '<h1>' . $e->getFile() . ' line: ' . $e->getLine() . '</h1>';
+        $errMsg .= '<h1>' . $e->getMessage() . '</h1>';
+        $errMsg .= '</center></body></html>';
+ 
+        $data = array(
+            'ret' => 1,
+            'msg' => $e->getMessage(),
+            'data' => [],
+            'timestamp' => time(),
+        );
+        //echo $errMsg; 
+        echo json_encode($data);
     }
 }
-
-foreach ($_GET as $k => $v)
-{
-    if (is_string($v))
-    {
-        $_GET[$k] = addslashes($v);
-    }
-}
-
-include FRAMEWORK_PATH . 'Ngx.php';
-Ngx\Ngx::getInstance()
-    ->init(APPLICATION_PATH . 'Config/Main.php')
-    ->run($_SERVER['PATH_INFO']);
+$t1=microtime(true); //获取程序1，开始的时间 
+_main();
+$t2=microtime(true); //获取程序1，结束的时间
+echo $t2-$t1;
